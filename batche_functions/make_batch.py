a9cf340d6b8fb45036a9a5b3b66ec80caa3990cb
jaisouffle <jaisoofi@gmail.com>
2025-04-16 03:37:01 -0400

    yeast, genes, format

--- batche_functions/make_batch.py
+++ batche_functions/make_batch.py
@@ -10,17 +10,40 @@ complexes = [
 ]
 
 prompt_techniques = {
-    "zero-shot": "For each of the following protein {complex}, provide the role of the protein complex by labelling it as 'Complex Function', but avoid overgeneralization and long answers. Give the organism it belongs to (human, mouse/rat, C.elegans, or Drosophila melanogaster) by labelling it as 'Organism'. Additionally, include a list of proteins the {complex} consists of and label the list 'Proteins'.",
-    
-    "few-shot": """For each of the following protein {complex}, provide the role of the protein complex (avoid generalization and long answers) and the organism it belongs to (human, mouse/rat, C.elegans, or Drosophila melanogaster). Additionally, include a list of proteins the {complex} consists of. Here are an example of the output:
-    'Complex Function: This is a part of the larger ATP4 or H+/K+ ATPase complex, a proton pump responsible for gastric acid secretion in the stomach.' 
-    'Organism: Mouse
-    'Proteins: ATP4A, ATP4B """, 
+    "zero-shot": """For each of the following protein {complex}, provide the role of the protein complex by 
+    labelling it as 'Complex Function', but avoid overgeneralization and long answers. Give the organism it 
+    belongs to (Human, Mouse, Caenorhabditis elegans, Drosophila melanogaster, or Saccharomyces cerevisiae) 
+    by labelling it as 'Organism'. Additionally, include a list of proteins the {complex} consists of and label 
+    the list 'Proteins'. Additionally, include a list of the corresponding genes the proteins belong to and 
+    label this list 'Genes' """,
+     
+    "few-shot": """For each of the following protein {complex}, provide the role of the protein complex (avoid 
+    generalization and long answers) and the organism it belongs to (Human, Mouse, Caenorhabditis elegans, 
+    Drosophila melanogaster, or Saccharomyces cerevisiae). Additionally, include a list of proteins the {complex} 
+    consists of. Additionally, include a list of corresponding genes for the proteins. Here is an example of the 
+    output for one complex:
+
+    'Complex Name: ATP4A-ATP4B complex'
+    'Complex Function: This is a part of the larger ATP4 or H+/K+ ATPase complex, a proton pump responsible 
+    for gastric acid secretion in the stomach.' 
+    'Organism: Human'
+    'Proteins: Potassium-transporting ATPase alpha chain 1, Potassium-transporting ATPase subunit beta' 
+    'Genes: ATP4A(Human), ATP4B(Human)' """, 
             
-    "contextual": """You are an expert in the field of biology and molecular machines. For each of the following protein {complex}, provide the role of the protein complex (avoid generalization and long answers) and the organism it belongs to (human, mouse/rat, C.elegans, or Drosophila melanogaster). Additionally, include a list of proteins the {complex} consists of. Here is an example of the output: 
-    'Complex Function: This is a part of the larger ATP4 or H+/K+ ATPase complex, a proton pump responsible for gastric acid secretion in the stomach.' 
-    'Organism: Mouse
-    'Proteins: ATP4A, ATP4B"""
+    "contextual": """You are an expert in the field of biology and molecular machines. For each of the following 
+    protein {complex}, provide the role of the protein complex (avoid generalization and long answers) and the 
+    organism it belongs to (Human, Mouse, Caenorhabditis elegans, Drosophila melanogaster, or Saccharomyces 
+    cerevisiae). Additionally, include a list of proteins the {complex} consists of. Additionally, include a 
+    list of corresponding genes for the proteins. Here is an example of 
+    the output for one complex:
+
+    'Complex Name: ATP4A-ATP4B complex'
+    'Complex Function: This is a part of the larger ATP4 or H+/K+ ATPase complex, a proton pump responsible 
+    for gastric acid secretion in the stomach.' 
+    'Organism: Human'
+    'Proteins: Potassium-transporting ATPase alpha chain 1, Potassium-transporting ATPase subunit beta' 
+    'Genes: ATP4A(Human), ATP4B(Human)' """, 
+    
 }
 
 # make jsonl file
